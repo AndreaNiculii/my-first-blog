37c24c8cd8f419ce7f7099f7d35571f8de33b1a5
AndreaNiculii <andreaniculii@gmail.com>
2025-04-16 01:40:35 +0300

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +131,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
